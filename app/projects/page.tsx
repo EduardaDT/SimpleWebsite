4ad5dee1789041232da3bd8e36d0f1a6c69e2bff
Maria Eduarda <mariaeduarda.dionisio@gmail.com>
2023-11-27 20:46:13 -0300

    add project

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -14,6 +14,13 @@ const topics = [
     githubLink:
       "https://github.com/EduardaDT/ft_printf"
   },
+  {
+    title: "get_next_line",
+    content:
+      "get_next_line is a function that reads one line at a time from a file descriptor, whether it's a file, standard input, or any other stream.",
+    githubLink:
+      "https://github.com/EduardaDT/get_next_line"
+  },
 ];
 
 export default function Example() {
@@ -25,12 +32,14 @@ export default function Example() {
         <div className="mt-8 space-y-8">
           {topics.map((topic, index) => (
             <Link href={topic.githubLink} key={index} className="text-base duration-500 text-blue-500 hover:text-blue-300">
+              <div className="mb-8">
                 <Card>
                   <div className="p-6">
                     <h2 className="text-2xl font-bold mb-4 text-blue-200">{topic.title}</h2>
                     <p className="text-blue-300">{topic.content}</p>
                   </div>
                 </Card>
+              </div>
             </Link>
           ))}
         </div>
